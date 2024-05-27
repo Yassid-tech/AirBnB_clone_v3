d30d628eeb89e50431826381724f8802747ce069
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-05-27 16:59:50 -0700

    Fixing bugs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,10 +6,12 @@ from flask import jsonify
 from models import storage
 from api.v1.views import app_views
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns status in JSON format"""
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
